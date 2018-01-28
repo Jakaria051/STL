6137b6ddbb952828f67bd54c96da55bfc1000720
Jakaria Ahmed Chowdhury <32171981+Jakaria051@users.noreply.github.com>
2018-01-28 20:22:50 +0600

    Add files via upload

---
+++ Pair.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //1st rules
+    pair<int, char>p;
+    p.first = 10;
+    p.second = 'A';
+    cout<<p.first<<" "<<p.second<<endl;
+
+    //second alternative rules
+
+    pair<int ,char>p1(10,'B');
+    cout<<p1.first<<" "<<p1.second<<endl;
+
+    //third  alternative rules
+
+    pair < int, int>p2;
+    p2 = make_pair(20,30);
+     cout<<p2.first<<" "<<p2.second<<endl;
+
+     //fourth alternative rules
+    pair < string, pair<int,char> >student;
+    student = make_pair("jakaria",make_pair(051,'J'));
+    cout<<student.first<<" "<<student.second.first<<" "<<student.second.second<<endl;
+
+    return 0;
+}
